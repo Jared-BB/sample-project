d1a417d6c28296cf1b0edc16f5022f15a8e354b7
Jared <jared.barber@hotmail.com>
2026-07-16 11:54:54 +0200

    add more tests & fixes on pipeline

--- tests/User/Integration/SendWelcomeEmailToUserCommandHandlerTest.php
+++ tests/User/Integration/SendWelcomeEmailToUserCommandHandlerTest.php
@@ -5,9 +5,12 @@ namespace App\Tests\User\Integration;
 use App\Tests\IntegrationTestCase;
 use App\Tests\Story\User\UserStory;
 use App\User\Application\Command\SendWelcomeEmailToUserCommand;
+use App\User\Domain\Exception\UserNotFoundException;
 use App\User\Domain\User;
 use Symfony\Bundle\FrameworkBundle\Test\MailerAssertionsTrait;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Uid\Uuid;
 
 class SendWelcomeEmailToUserCommandHandlerTest extends IntegrationTestCase
 {
@@ -33,5 +36,24 @@ class SendWelcomeEmailToUserCommandHandlerTest extends IntegrationTestCase
 
         $this->assertEmailAddressContains($email, 'To', $user->email()->asString());
         $this->assertEmailTextBodyContains($email, 'Welcome');
+    }
+
+    public function test_send_welcome_email_when_user_not_found(): void
+    {
+        /** @var MessageBusInterface $bus */
+        $bus = self::getContainer()->get('commands.bus');
+
+        try {
+            $bus->dispatch(
+                new SendWelcomeEmailToUserCommand(
+                    userId: Uuid::v7(),
+                ),
+            );
+        } catch (HandlerFailedException $exception) {
+            self::assertInstanceOf(
+                UserNotFoundException::class,
+                $exception->getPrevious(),
+            );
+        }
     }
 }
